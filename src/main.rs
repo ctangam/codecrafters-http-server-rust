19bb5657338cd40e542681cb22e8f5eedf99f68c
汤淳 <tangchun@szcgc.com>
2023-11-30 16:22:58 +0800

    pass

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 use std::net::TcpListener;
 use std::io::prelude::*;
 
+use itertools::Itertools;
+
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -13,9 +16,24 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(response.as_bytes()).unwrap();
+                let mut buffer = String::new();
+                stream.read_to_string(&mut buffer).unwrap();
+                println!("request: {}", buffer);
+
+                let start_line = buffer.lines().next().unwrap();
+                let (method, path, _) = start_line.split_whitespace().collect_tuple().unwrap();
+                println!("method: {}, path: {}", method, path);
+
+                match path {
+                    "/" => {
+                        let response = "HTTP/1.1 200 OK\r\n\r\n";
+                        stream.write_all(response.as_bytes()).unwrap();
+                    }
+                    _ => {
+                        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                        stream.write_all(response.as_bytes()).unwrap();
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
