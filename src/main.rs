34fe6bab50d89f5f0263b8274c9df2cbc5141441
汤淳 <tangchun@szcgc.com>
2024-06-26 10:33:30 +0800

    add echo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,90 @@
 use std::collections::HashMap;
+use std::fmt::Formatter;
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 use std::io::prelude::*;
+use std::str::FromStr;
 
+use anyhow::Error;
 use itertools::Itertools;
 
+enum Method {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    HEAD,
+}
+
+impl Method {
+    fn from_str(s: &str) -> Option<Method> {
+        match s {
+            "GET" => Some(Method::GET),
+            "POST" => Some(Method::POST),
+            "PUT" => Some(Method::PUT),
+            "PATCH" => Some(Method::PATCH),
+            "DELETE" => Some(Method::DELETE),
+            "OPTIONS" => Some(Method::OPTIONS),
+            "HEAD" => Some(Method::HEAD),
+            _ => None,
+        }
+    }
+}
+
+struct Request {
+    method: Method,
+    path: String,
+    version: String,
+    headers: HashMap<String, String>,
+    body: Vec<u8>,
+}
+
+impl FromStr for Request {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (request_line, remained) = s.split_once("\r\n").unwrap();
+        let mut request_line = request_line.split_whitespace().take(3);
+        let method = request_line.next().unwrap();
+        let path = request_line.next().unwrap();
+        let version = request_line.next().unwrap();
+
+        let (headers, body) = remained.split_once("\r\n\r\n").unwrap();
+        let headers: HashMap<String, String> = headers.split("\r\n").map(|h| {
+            let (h, v) = h.split_once(": ").unwrap();
+            (h.to_string(), v.to_string())
+        }).collect();
+
+        Ok(Request {
+            method: Method::from_str(method).unwrap(),
+            path: path.to_string(),
+            version: version.to_string(),
+            headers,
+            body: body.as_bytes().to_vec(),
+        })
+    }
+}
+
+struct Response {
+    version: String,
+    status: String,
+    headers: HashMap<String, String>,
+    body: Vec<u8>,
+}
+
+impl ToString for Response {
+    fn to_string(&self) -> String {
+        let mut response = String::new();
+        response.push_str(&format!("{} {}\r\n", self.version, self.status));
+        for (k, v) in &self.headers {
+            response.push_str(&format!("{}: {}\r\n", k, v));
+        }
+        response.push_str("\r\n");
+        response.push_str(&String::from_utf8_lossy(&self.body));
+        response
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -22,21 +102,27 @@ fn main() {
                 let buffer = String::from_utf8_lossy(&buffer);
                 println!("request: {}", buffer);
 
-                let (request_line, remained) = buffer.split_once("\r\n").unwrap();
-                let mut request_line = request_line.split_whitespace().take(3);
-                let method = request_line.next().unwrap();
-                let path = request_line.next().unwrap();
-                let version = request_line.next().unwrap();
+                let request = Request::from_str(&buffer).unwrap();
 
-                let (headers, body) = remained.split_once("\r\n\r\n").unwrap();
-                let headers: HashMap<&str, &str> = headers.split("\r\n").map(|h| {
-                    h.split_once(": ").unwrap()
-                }).collect();
-
-                match path {
+                match &request.path[..] {
                     "/" => {
                         let response = "HTTP/1.1 200 OK\r\n\r\n";
                         stream.write_all(response.as_bytes()).unwrap();
+                    }
+                    path if path.starts_with("/echo") => {
+                        let uri = path.strip_prefix("/echo/").unwrap();
+                        
+                        let mut headers = HashMap::new();
+                        headers.insert("Content-Type", "text/plain".to_string());
+                        headers.insert("Content-Length", uri.len().to_string());
+                        let response = Response {
+                            version: request.version,
+                            status: "200 OK".to_string(),
+                            headers: HashMap::new(),
+                            body: uri.as_bytes().to_vec(),
+                        };
+
+                        stream.write_all(&response.to_string().as_bytes()).unwrap();
                     }
                     _ => {
                         let response = "HTTP/1.1 404 Not Found\r\n\r\n";
